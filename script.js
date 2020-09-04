0f19a06be388deb7c8b55b4ad0388bac50b16bf9
Jude Clark <judeclark2019@gmail.com>
2020-09-04 19:49:33 -0400

    added click listener to the start button

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+//DOM variables
+var startButtonEl = document.getElementById("start-button");
+
+// on button click, hide <p>s
+startButtonEl.addEventListener("click", function(event){
+    console.log("clicked start quiz button")
+})
