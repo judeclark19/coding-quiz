bec1ee76f892eaa47d9427df154eedb6fba8f4ba
Jude Clark <judeclark2019@gmail.com>
2020-09-04 20:07:18 -0400

    clicking the start button now removes the intro paragraphs

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 //DOM variables
 var startButtonEl = document.getElementById("start-button");
 // var instructionParagraphs = document.getElementsByClassName("instructions");
-var instructionParagraphs = document.querySelector(".instructions");
+var instructionParagraphs = document.querySelectorAll(".instructions");
 
 // on button click, hide <p>s
 startButtonEl.addEventListener("click", function(event){
-    instructionParagraphs.setAttribute("style", "visibility:hidden")
+    // instructionParagraphs.setAttribute("style", "visibility:hidden")
+//    instructionParagraphs.style.visibility="blue";
+console.log(instructionParagraphs);
+    for (let i=0; i<instructionParagraphs.length; i++) {
+        instructionParagraphs[i].remove();
+    }
 })
