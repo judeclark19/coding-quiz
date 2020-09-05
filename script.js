781c2f101049d441aa2ae56858e04aba592f468f
Jude Clark <judeclark2019@gmail.com>
2020-09-04 21:12:30 -0400

    set up retrieval of question prompt text from DOM

--- script.js
+++ script.js
@@ -5,49 +5,57 @@ var quizDisplayCell = document.getElementById("quiz-display-cell");
 
 //Quiz Questions
 var questions = [
-    {"questionNumber": 1,
-        "questionPrompt": "Commonly used data types DO NOT include:",
-        "optionOne": "strings",
-        "optionTwo": "booleans",
-        "optionThree": "alerts",
-        "optionFour": "numbers"
-    },
-    //question2
-    //question3
-    //question4
-    //question5
-]
+  {
+    questionNumber: 1,
+    questionPrompt: "Commonly used data types DO NOT include:",
+    optionOne: "strings",
+    optionTwo: "booleans",
+    optionThree: "alerts",
+    optionFour: "numbers",
+  },
+  //question2
+  //question3
+  //question4
+  //question5
+];
 
 //When start button is clicked:
-// hide intro text
-startButtonEl.addEventListener("click", function(event){
-console.log(instructionParagraphs);
-    for (let i=0; i<instructionParagraphs.length; i++) {
-        instructionParagraphs[i].remove();
-        startButtonEl.remove();
-    }
-})
-//introducing first question <p> TODO: tie both functions to same click event
-startButtonEl.addEventListener("click", function(event){
-    //create an h3 for the prompt
-    var questionPrompt = document.createElement("h3");
-    //add text to the h3
-    questionPrompt.textContent = "first question prompt";
-    //append it inside quiz-display-cell
-    quizDisplayCell.appendChild(questionPrompt)
+// TODO: how do I combine these?
+startButtonEl.addEventListener("click", hideInstructions);
+startButtonEl.addEventListener("click", showNextPrompt);
+startButtonEl.addEventListener("click", showOptionOne);
+startButtonEl.addEventListener("click", showOptionTwo);
 
-    var optionOne = document.createElement("button");
-    //add text to the span
-    optionOne.textContent = "first option";
-    optionOne.classList.add("answer-button", "btn", "btn-info");
-    //append it inside quiz-display-cell
-    quizDisplayCell.appendChild(optionOne)
+function hideInstructions() {
+  for (let i = 0; i < instructionParagraphs.length; i++) {
+    instructionParagraphs[i].remove();
+    startButtonEl.remove();
+  }
+}
 
-    var optionTwo = document.createElement("button");
-    //add text to the span
-    optionTwo.textContent = "second option";
-    optionTwo.classList.add("answer-button", "btn", "btn-info");
-    //append it inside quiz-display-cell
-    quizDisplayCell.appendChild(optionTwo)
+function showNextPrompt() {
+  //create an h3 for the prompt
+  var questionPrompt = document.createElement("h3");
+  //add text to the h3
+  questionPrompt.textContent = questions[0].questionPrompt;
+  //append it inside quiz-display-cell
+  quizDisplayCell.appendChild(questionPrompt);
+}
 
-    })
+function showOptionOne() {
+  var optionOne = document.createElement("button");
+  //add text to the span
+  optionOne.textContent = "first option";
+  optionOne.classList.add("answer-button", "btn", "btn-info");
+  //append it inside quiz-display-cell
+  quizDisplayCell.appendChild(optionOne);
+}
+
+function showOptionTwo() {
+  var optionTwo = document.createElement("button");
+  //add text to the span
+  optionTwo.textContent = "second option";
+  optionTwo.classList.add("answer-button", "btn", "btn-info");
+  //append it inside quiz-display-cell
+  quizDisplayCell.appendChild(optionTwo);
+}
