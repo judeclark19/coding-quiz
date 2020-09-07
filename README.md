92abd4b0b9b9bc53b1a37d183904b33ac18d5237
Jude Clark <judeclark2019@gmail.com>
2020-09-07 12:06:18 -0400

    new scores append to the scoreboard now but they don't maintain after refresh

--- README.md
+++ README.md
@@ -7,6 +7,9 @@
 *[Simple Countdown Timer with JavaScript](https://www.youtube.com/watch?v=x7WJEmxNlEs)
 
 
+## Bugs
+If the duration of the game in minutes is >=1, the game over function will run when you complete the quiz and again when the timer finally runs out. This is because the timer keeps counting down after the game ends
+
 ## Answer Key
 <details>
   <summary>Spoiler alert! Click to reveal question and answer key.</summary>
--- script.js
+++ script.js
@@ -7,13 +7,14 @@ const questionPromptEl = document.getElementById("question-prompt-text");
 const answerButtonsDiv = document.getElementById("answer-buttons");
 const questionsRight = document.getElementById("questions-right");
 const totalQuestionsEl = document.getElementById("total-questions");
-const timeRemainderEl =document.getElementById("time-remainder");
+const timeRemainderEl = document.getElementById("time-remainder");
 const gameOverModal = document.getElementById("game-over-mod-bg");
 const gomCloseButton = document.getElementById("gom-close-button");
 const scoreboardModal = document.getElementById("scoreboard-mod-bg");
 const scoreModCloseButton = document.getElementById("scoreMod-close-button");
 const scoreSubmitButton = document.querySelector(".submit-btn");
 const nameInputField = document.getElementById("name-input-field");
+const scoreTable = document.getElementById("add-new-scores-here");
 // const timer = document.getElementById("timer-readout");
 
 //Question and answer bank
@@ -70,14 +71,14 @@ const quizQuestions = [
 //Other variables
 var currentQuestionIndex = 0;
 var totalCorrect = 0;
-const gameDurationInMin = 0.1;
+const gameDurationInMin = 1;
 let gameDurationInSec = gameDurationInMin * 60;
 const totalQuestions = quizQuestions.length;
 
 //Event listeners
 viewScoreboard.addEventListener("click", () => {
   scoreboardModal.classList.add("modal-active");
-})
+});
 startButton.addEventListener("click", startGame);
 nextButton.addEventListener("click", () => {
   currentQuestionIndex++;
@@ -93,6 +94,7 @@ scoreModCloseButton.addEventListener("click", () => {
 scoreSubmitButton.addEventListener("click", () => {
   gameOverModal.classList.remove("modal-active");
   scoreboardModal.classList.add("modal-active");
+  submitScore();
 });
 
 //Functions
@@ -100,6 +102,7 @@ scoreSubmitButton.addEventListener("click", () => {
 //start the game, show first question
 function startGame() {
   currentQuestionIndex = 0;
+  totalCorrect = 0;
   resetState();
   startButton.classList.add("hide");
   introSlide.classList.add("hide");
@@ -108,31 +111,29 @@ function startGame() {
   startTimer();
 }
 //Start the timer
-function startTimer(){
-
+function startTimer() {
   var timer = document.createElement("div");
-timer.setAttribute("id", "timer-readout");
-document.body.appendChild(timer);
+  timer.setAttribute("id", "timer-readout");
+  document.body.appendChild(timer);
 
   gameDurationInSec = gameDurationInMin * 60;
-  timer.innerHTML = "BEGIN!"
-var myInterval = setInterval(() => {
+  timer.innerHTML = "BEGIN!";
+  var myInterval = setInterval(() => {
+    gameDurationInSec--;
 
-  gameDurationInSec--;
+    let minuteHand = Math.floor(gameDurationInSec / 60);
+    let secondHand = gameDurationInSec % 60;
+    if (secondHand < 10) {
+      secondHand = "0" + secondHand;
+    }
 
-  let minuteHand = Math.floor(gameDurationInSec / 60);
-  let secondHand = gameDurationInSec % 60;
-  if (secondHand < 10) {
-         secondHand = "0" + secondHand;
-       }
+    timer.innerHTML = `${minuteHand}:${secondHand}`;
 
-       timer.innerHTML = `${minuteHand}:${secondHand}`;
-
-  if (gameDurationInSec <= 0){
-    clearInterval(myInterval);
-    gameOver();
-  }
-}, (1000));
+    if (gameDurationInSec <= 0) {
+      clearInterval(myInterval);
+      gameOver();
+    }
+  }, 1000);
 }
 
 //Show next question once the game is already underway
@@ -185,6 +186,32 @@ function gameOver() {
   questionsRight.innerHTML = totalCorrect;
   totalQuestionsEl.innerHTML = totalQuestions;
   timeRemainderEl.innerHTML = gameDurationInSec;
+}
+
+function submitScore () {
+  localStorage.setItem("myName", nameInputField.value)
+  localStorage.setItem("myScore", (totalCorrect/totalQuestions)*100+"%")
+  localStorage.setItem("myTime", gameDurationInSec);
+  console.log(localStorage);
+
+var scoreTableRow = document.createElement("tr");
+
+  var nameField = document.createElement("th")
+  nameField.setAttribute("scope", "row");
+  nameField.textContent = localStorage.getItem("myName");
+  scoreTableRow.appendChild(nameField);
+
+  var percentField = document.createElement("td")
+  // percentField.setAttribute("scope", "row");
+  percentField.textContent = localStorage.getItem("myScore");
+  scoreTableRow.appendChild(percentField);
+
+  var timeField = document.createElement("td")
+  // timeField.setAttribute("scope", "row");
+  timeField.textContent = localStorage.getItem("myTime");
+  scoreTableRow.appendChild(timeField);
+
+  scoreTable.appendChild(scoreTableRow);
 }
 
 //showing shows right answer after the user has guessed
